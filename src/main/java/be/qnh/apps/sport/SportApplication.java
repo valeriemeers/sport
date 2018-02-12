1cf44b7361c6922c5171480272ad2b2360e2e202
Valerie Meers <valerie.meers@qnh.be>
2018-02-12 11:08:26 +0100

    Initial commit

---
+++ src/main/java/be/qnh/apps/sport/SportApplication.java
@@ -0,0 +1,12 @@
+package be.qnh.apps.sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportApplication.class, args);
+	}
+}
